4718708d21d6bf14af22b85b54971a087521bae0
dinhthigaamer <longle336699@gmail.com>
2026-03-10 16:38:15 +0700

    Thi's solution

--- Prob8_Thi.cpp
+++ Prob8_Thi.cpp
@@ -58,12 +58,34 @@ int swap_point(const vector<int> &path, int dist, int p1, int p2)
     return new_dist;
 }
 
+pair<int, vector<int>> insert_point(int p, vector<int> path)
+{
+    path.push_back(p);
+    int dist = distance(path);
+    vector<int> tmp_path = path;
+
+    for (int i = path.size() - 2; i > 0; --i)
+    {
+        swap(path[i + 1], path[i]);
+        int new_dist = distance(path);
+
+        if (new_dist < dist)
+        {
+            dist = new_dist;
+            tmp_path = path;
+        }
+    }
+    path = tmp_path;
+
+    return {dist, path};
+}
 // Insert new point to current path, return the shortest distance can be found and the new path
 // If cannot find a new path < limit, return {-1, {}}
 pair<int, vector<int>> insert_point_to_path(int p, vector<int> path, int limit = 0)
 {
-    path.push_back(p);
-    int dist = distance(path);
+    pair<int, vector<int>> tmp = insert_point(p, path);
+    int dist = tmp.first;
+    path = tmp.second;
 
     while (1)
     {
@@ -93,6 +115,64 @@ pair<int, vector<int>> insert_point_to_path(int p, vector<int> path, int limit =
     return {dist, path};
 }
 
+// Try to decrease the longest path
+// by removing some point and adding it to the shortest path
+void decrese_longest_path()
+{
+    while (1)
+    {
+        int better = 0;
+
+        int max_dist = 0, min_dist = 1e9;
+        int max_truck = 1, min_truck = 1;
+
+        for (int x = 1; x <= k; ++x)
+        {
+            int dist = distance(paths[x]);
+            if (max_dist < dist)
+            {
+                max_dist = dist;
+                max_truck = x;
+            }
+
+            if (min_dist > dist)
+            {
+                min_dist = dist;
+                min_truck = x;
+            }
+        }
+
+        vector<int> &tmp = paths[max_truck];
+
+        for (int i = 1; i < tmp.size() - 1; ++i)
+        {
+            // remove point paths[max_truck][i]
+            int new_dist = max_dist - (d[tmp[i - 1]][tmp[i]] + d[tmp[i]][tmp[i + 1]]);
+            new_dist += d[tmp[i - 1]][tmp[i + 1]];
+
+            if (new_dist >= max_dist)
+                continue;
+
+            for (int x = 1; x <= k; ++x)
+            {
+                if (x == max_truck)
+                    continue;
+                pair<int, vector<int>> res = insert_point(tmp[i], paths[x]);
+                if (res.first >= max_dist)
+                    continue;
+
+                tmp.erase(tmp.begin() + i);
+                paths[x] = res.second;
+                better = 1;
+                break;
+            }
+        }
+
+        if (better == 0)
+            break;
+    }
+}
+
 void solve()
 {
     for (int p = 1; p <= n; ++p)
@@ -114,6 +194,8 @@ void solve()
 
         paths[x] = tmp_path;
     }
+
+    decrese_longest_path();
 }
 
 int main()
